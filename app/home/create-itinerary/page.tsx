f3adedc5897e312a3f06e6fc4a8e53f33131e9a2
mhkerchen <mhkerchen@gmail.com>
2024-04-01 13:35:08 -0400

    Created basic itinerary creation form

---
+++ app/home/create-itinerary/page.tsx
@@ -0,0 +1,70 @@
+import '../../style.css';
+import EventParticipantsManager from '../../client/eventParticipantsManager'
+import ParticipantsBox from '../../client/participantsBox'
+import {AddNameBubble, NameBubble} from '../../client/participantHelpers'
+
+
+export default async function Page() {
+	return (	
+		<NewItineraryScreen/>
+  );
+	
+}
+
+
+
+function NewItineraryScreen() {
+	return (
+
+		<div>
+			<h1>Create a new itinerary</h1>
+
+			<p>Name:</p>
+			<input></input>
+			<p>Participants:</p>
+			<EventParticipantsManager eventParticipants={["JohnDoe"]}/>
+
+			<button>Submit</button>
+		</div>
+	);
+	}
+
+
+	
+// Avoid caching, so that hot updates work as expected
+export const dynamic = 'force-dynamic'
+// Get the language data from the database.
+// Returns a json object.
+
+async function getUserData() {
+    try {
+        const res = await fetch(
+			//'http://cs-vm-02.cs.mtholyoke.edu:31600/api'
+			'http://localhost:31600/users',
+			);
+        console.log('Frontend Fetch: Response status:', res.status);
+        const data = await res.json();
+        console.log('Frontend Fetch: Data from server:', data);
+        return data;
+    } catch (error) {
+        console.error('Frontend Fetch: Error fetching data:', error);
+        throw error;
+    }
+}
+
+async function getEventData() {
+    try {
+        const res = await fetch(
+			//'http://cs-vm-02.cs.mtholyoke.edu:31600/api'
+			'http://localhost:31600/events',
+			);
+        console.log('Frontend Fetch: Response status:', res.status);
+        const data = await res.json();
+        console.log('Frontend Fetch: Data from server:', data);
+        return data;
+    } catch (error) {
+        console.error('Frontend Fetch: Error fetching data:', error);
+        throw error;
+    }
+}
+
