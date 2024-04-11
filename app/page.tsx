c36ff0fa1153f9ca1eb186d1ec83e45b4f150f02
mhkerchen <mhkerchen@gmail.com>
2024-04-11 15:35:52 -0400

    Added skeletons for login pages (not placed)

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,51 @@ import {OpenAddDialogue} from './client/addToItineraryClient'
 
 export default async function Page() {
 	return (	
-		<SearchScreen/>
+		<LoginScreen/>
   );
 
 }
+
+function LoginScreen() {
+	return (
+		<div>
+			<form onSubmit={null}>
+				<p>Username:</p>
+				<input type="text"></input>
+				<p>Password:</p>
+				<input type="text"></input>
+			</form>
+
+			<button>Submit</button>
+		</div>
+		
+	)
+}
+
+function ErrorScreenUsername() {
+	return (
+		<div>
+			<p>Error: Username not found.</p>
+			<button>Return to login</button>
+		</div>
+	)
+}
+
+
+function ErrorScreenPassword() {
+	return (
+		<div>
+			<p>Error: Password is incorrect.</p>
+			<button>Return to login</button>
+		</div>
+	)
+}
+
+
+
+
+
+
 
 
 /**
