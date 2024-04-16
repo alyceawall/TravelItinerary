4fc67bcf94a812c115c30f2ca8b9088c81df16cc
alyceawall <113461114+alyceawall@users.noreply.github.com>
2024-04-16 14:06:26 -0400

    Update db.ts

--- app/api/db.ts
+++ app/api/db.ts
@@ -1,10 +1,11 @@
 import mongoose from 'mongoose';
-// import userModel from './userModel';
-// import itineraryModel from './itineraryModel';
-// import eventModel from './eventModel';
+import userModel from './userModel';
+import itineraryModel from './itineraryModel';
+import eventModel from './eventModel';
+import { addFriend } from './addFriends';
 
 // Connect to the database
-mongoose.connect('mongodb+srv://316:316Password@lbt.5zbr4zu.mongodb.net/LBT?retryWrites=true&w=majority');
+mongoose.connect(process.env.MONGODB_URI!, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 mongoose.connection.on('connected', () => {
@@ -15,40 +16,49 @@ mongoose.connection.on('error', (err) => {
   console.error('Error connecting to MongoDB:', err);
 });
 
-// Define a function to create test data
+//Define a function to create test data
 // async function createTestData() {
 //   try {
 //       // Create a test user
 //       const testUser = await userModel.create({
-//           name: 'John Doe',
-//           username: 'johndoe',
-//           password: 'testpassword',
-//           email: 'johndoe@example.com',
+//           name: 'Mario Bro',
+//           username: 'mariobro',
+//           password: 'mariopassword',
+//           email: 'mariobro@example.com',
 //           itineraries: [],
 //           friends: [],
 //       });
 
+//       const testUser2 = await userModel.create({
+//         name: 'Luigi Bro',
+//         username: 'luigibro',
+//         password: 'luigipassword',
+//         email: 'luigibro@example.com',
+//         itineraries: [],
+//         friends: [],
+//     });
+
 //       // Create a test itinerary
 //       const testItinerary = await itineraryModel.create({
-//           name: 'Test Itinerary',
-//           display_name: 'Test Itinerary',
+//           name: 'Vacation at Peach\'s Castle',
+//           display_name: 'Vacation at Peach\'s Castle',
 //           events: [], // Events are added separately
-//           date_start: new Date(),
-//           date_end: new Date(),
-//           participants: [testUser._id], // Add the test user as a participant
-//           desc: 'This is a test itinerary',
+//           date_start: new Date('2025-02-12T12:30:00'),
+//           date_end: new Date('2025-02-22T12:30:00'),
+//           participants: [testUser._id, testUser2._id], // Add the test user as a participant
+//           desc: 'Mario & Luigi\'s Trip to Peach\'s Castle.',
 //       });
 
 //       // Create a test event
 //       const testEvent = await eventModel.create({
-//           name: 'Test Event',
-//           display_name: 'Test Event',
-//           time_start: new Date(),
-//           time_end: new Date(),
-//           location: 'Test Location',
-//           link_to_site: 'https://bobby-tables.com/',
+//           name: 'Dinner with Peach',
+//           display_name: 'Dinner with Peach',
+//           time_start: new Date('2025-02-14T18:30:00'),
+//           time_end: new Date('2025-02-14T19:30:00'),
+//           location: 'Peach\'s Castle',
+//           link_to_site: '',
 //           participants: [testUser._id], // Add the test user as a participant
-//           desc: 'This is a test event',
+//           desc: 'Dinner with Peach',
 //       });
 
 //       // Associate the test event with the test itinerary
@@ -61,5 +71,15 @@ mongoose.connection.on('error', (err) => {
 //   }
 // }
 
-// // Call the function to create test data
-// createTestData();
+async function friends() {
+  try {
+    await addFriend();
+  } catch (error) {
+    console.error('Error adding friend:', error);
+  }
+}
+
+friends();
+
+// Call the function to create test data
+//createTestData();
